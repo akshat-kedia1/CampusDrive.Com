9fc9bdfee2134656d11a483efed6af4a96957285
abhiyadav-2307 <abhi1611919@gmail.com>
2025-06-22 13:31:46 +0200

    Captain profile

--- Backend/app.js
+++ Backend/app.js
@@ -1,22 +1,26 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const connectToDb = require('./db/db.js');
-const userRoutes = require('./routes/user.routes.js');
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./db/db.js");
+const userRoutes = require("./routes/user.routes.js");
+const captainRoutes = require("./routes/captain.routes.js");
 
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
+app.use("/captain", captainRoutes);
 
 module.exports = app;
 //hell HELLO
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service.js');
 const { validationResult } = require('express-validator');
+const blackListTokenModel = require('../models/blacklistToken.model.js');
 
 
 module.exports.registerUser = async (req, res, next) => {
@@ -10,6 +11,13 @@ module.exports.registerUser = async (req, res, next) => {
         return res.status(400).json({ errors: errors.array() });
     }
     const { fullname, email, password } = req.body;
+
+    const isUserAlreadyExist = await user.findOne({ email });
+    
+        if (isUserAlreadyExist) {
+            return res.status(400).json({ message: 'User already exist' });
+        }
+
     const hashedPassword = await userModel.hashPassword(password);
 
     // if(!firstname) console.log("Gadbad hai");
@@ -22,4 +30,44 @@ module.exports.registerUser = async (req, res, next) => {
     });
     const token = user.generateAuthToken();
     res.status(201).json({ token, user });  
+}
+
+module.exports.loginUser = async (req, res, next)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(401).json({message: 'Invalid email or password'});
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({message: 'Invalid email or password'});
+    }
+
+    const token = user.generateAuthToken();
+
+
+    res.cookie('token', token);
+
+    res.status(200).json({token, user});
+}
+
+module.exports.getUerProfile = async(req,res,next)=>{
+    res.status(200).json(req.user);
+}
+
+module.exports.logout = async(req,res,next)=>{
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    await blackListTokenModel.create({token});
+    res.status(200).json({message: 'Logged out'});
 }
