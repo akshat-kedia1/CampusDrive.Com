3e2ebe9b17c51af6ab8bd9c5cb0e2d7e04767f83
abhiyadav-2307 <abhi1611919@gmail.com>
2025-06-25 15:36:51 +0530

    Captain signup, login, logout routes integrated with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,47 @@ import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
 import { UserDataContext } from './context/UserContext'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
+import CaptainHome from './pages/CaptainHome'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import CaptainLogout from './pages/CaptainLogout'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Start />} />
-        <Route path='/home' element={<Home />} />
+        <Route path='/home' element={
+          <UserProtectWrapper>
+            <Home />
+          </UserProtectWrapper>
+        } />
         <Route path='/signup' element={<UserSignup />} />
         <Route path='/login' element={<UserLogin />} />
+        <Route path='/user/logout' element={
+          <UserProtectWrapper>
+            <UserLogout />
+          </UserProtectWrapper>
+        } />
+        
+
         <Route path='/captain-login' element={<CaptainLogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+            <CaptainHome />
+          </CaptainProtectWrapper>
+        } /> 
+        <Route path='/captain-logout' element={
+          <CaptainProtectWrapper>
+            <CaptainLogout />
+          </CaptainProtectWrapper>
+        } />
 
+        
       </Routes>
+      
     </div>
   )
 }
